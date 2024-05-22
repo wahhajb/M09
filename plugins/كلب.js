a3f822a71874583519479f37216857610b66af4b
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-05-23 00:09:32 +0300

    Update كلب.js

--- plugins/كلب.js
+++ plugins/كلب.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, text }) => {
 try {
 let res = await fetch('https://api.thedogapi.com/v1/images/search')
 let img = await res.json()
-let caption = `『🔥┇MEGO-𝙱𝙾𝚃』`.trim()
+let caption = `『🔥┇𝑮𝒐𝒌𝒖-𝙱𝙾𝚃』`.trim()
 conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m)
 } catch {
 throw '*خـطـئ*'
