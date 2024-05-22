81f21be99a1d9a857605558d0404cd5447f0186d
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-05-23 01:17:16 +0300

    Update menu play.js

--- plugins/menu play.js
+++ plugins/menu play.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let videoUrl = 'https://telegra.ph/file/c1f3adc59818e62a90a8f.mp4'
+  let videoUrl = 'https://telegra.ph/file/d3f644175fc8ec076cb69.mp4'
   let { name } = global.db.data.users[who]
   m.react('🎮')
 let str = `                  ✥━─━⌬ 𝑮𝒐𝒌𝒖_𝒃𝒐𝒕 ⌬━─━✥
