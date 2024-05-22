6926b78b30afaabf31fa71b45ee9c475a49022e2
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-05-22 22:52:40 +0300

    Update transformador-togifaud.js

--- plugins/transformador-togifaud.js
+++ plugins/transformador-togifaud.js
@@ -4,7 +4,7 @@ if (!m.quoted) throw `*⚠️رد ع فيديو لتحويله جيفgif*`
 const q = m.quoted || m
 const mime = (q.msg || q).mimetype || ''
 if (!/(mp4)/.test(mime)) throw `*⚠️ تشابه ${mime} غير مدعوم*` 
-m.reply('*🚀 تم التحويل بوساطه ميجو*')
+m.reply('*🚀 تم التحويل بوساطه غوكو*')
 const media = await q.download()
 conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*🚀🚀*'}, {quoted: m})
 
