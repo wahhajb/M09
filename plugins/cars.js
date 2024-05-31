5045e5c4d6bc492b798cc8828e5c5ac713fa0394
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-01 00:05:34 +0300

    Create cars.js

---
+++ plugins/cars.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+let handler = async (m, { command, conn, usedPrefix }) => {
+  try {
+    let res = (await axios.get('https://api.popcat.xyz/car')).data;
+
+    if (!res || !res.image || !res.title) {
+      throw new Error('استجابة API غير صحيحة');
+    }
+
+    conn.sendFile(m.chat, res.image, 'car_image.jpg', `نـوع الـسياره🏎️:${res.title}`, m);
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, 'حدث خطأ أثناء جلب البيانات', m);
+  }
+};
+
+handler.command = handler.help = ['car', 'سياره'];
+handler.tags = ['car'];
+export default handler;
