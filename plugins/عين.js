36d2dc63a1372c3706bfd318b27d6149a60bf2a4
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-02 01:52:05 +0300

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -1,31 +1,32 @@
-let timeout = 60000
-let poin = 500
+let timeout = 40000
+let poin = 5000
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    conn.tokitoki = conn.tokitoki ? conn.tokitoki : {}
     let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+    if (id in conn.tokitoki) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tokitoki[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/Hema732828/test11/main/3en.json')).json()
-  let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*╭━━━[ *${command.toUpperCase()}* ]━━━━⬣
-┃❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
- *لو مش عارف الاجابه قول استخدم.معرفش*
-  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『𝗚𝗼𝗸𝘂-𝘽𝙊𝙏🩸』
-     `.trim()
-    conn.tebakbendera[id] = [
+    let src = await (await fetch('https://raw.githubusercontent.com/DK3MK/worker-bot/main/eye.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `
+*╮──────────────────⟢ـ*
+*❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)} ثـانـيـة┇❯*
+*❐↞┇الـجـائـزة💰↞ +${poin} ذهــب┇❯*
+*╯──────────────────⟢ـ*
+> *قم بالرد على الرسالة بالإجابة*
+> *إسـتـخـدم امـر [ .مـحـفـظـة ] للإستطلاع على الذهب الخاص بك*`.trim()
+    conn.tokitoki[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
-        json, poin,
+        json,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇╰━━━〔 *🛡️ 1.4.9* 〕━━━━━⬣`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
+            if (conn.tokitoki[id]) conn.reply(m.chat, `*❮ ⌛┇انتــهــى الــوقــت┇⌛❯*\n*❖↞┇الاجـابـة✅↞*  *${json.name}* *┇❯*`, conn.tokitoki[id][0])
+            delete conn.tokitoki[id]
         }, timeout)
     ]
 }
-handler.help = ['guessflag']
-handler.tags = ['game']
-handler.command = /^عين/i
+handler.help = ['عين']
+handler.tags = ['لعبة']
+handler.command = /^عين$/i
 
 export default handler
