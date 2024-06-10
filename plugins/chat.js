bec160cc28b07f806de58676dfd62380b05b81a5
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-10 21:48:16 +0300

    Create chat.js

---
+++ plugins/chat.js
@@ -0,0 +1,14 @@
+let handler = m => m 
+handler.all = async function (m) { 
+let chat = global.db.data.chats[m.chat] 
+
+if (/^مين عمك|مين عمك؟|من عمك/i.test(m.text) ) {
+     conn.reply(m.chat, `*عـمـرو ســـامـــا💜✨*\n*𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ*\n*wa.me/967774318278*`, m)
+}
+
+if (/^مين سيدك|مين سيدك؟|مين العم؟|مين العم|سيدك من/i.test(m.text) ) {
+     conn.reply(m.chat, `*عــمــرو ســـامـــا💜✨*\n\n`, m)
+}
+return !0 } 
+
+export default handler
