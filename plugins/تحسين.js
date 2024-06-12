f8de2bc6100a099921f20c49efe8b18afa835a49
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-12 22:09:04 +0300

    Create تحسين.js

---
+++ plugins/تحسين.js
@@ -0,0 +1,83 @@
+import FormData from "form-data";
+import Jimp from "jimp";
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  conn.hdr = conn.hdr ? conn.hdr : {}
+  if (m.sender in conn.hdr)
+    throw "هناك صوره لم تكتمل بعد اصبر"
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ""
+  if (!mime)
+    throw `هات الصوره يحب 🐦🤙🏻 `
+  if (!/image\/(jpe?g|png)/.test(mime))
+    throw `Mime ${mime} tidak support`
+  else conn.hdr[m.sender] = true;
+  m.reply("ويت 👀")
+  let img = await q.download?.()
+  let error
+  try {
+    const This = await processing(img, "enhance")
+    conn.sendFile(m.chat, This, "", "تفضل صوره ب جوده عاليه 🐦❤️", m)
+  } catch (er) {
+    error = true
+  } finally {
+    if (error) {
+      m.reply("error :(")
+    }
+    delete conn.hdr[m.sender]
+  }
+}
+
+handler.help = ['hd']
+handler.tags = ['tools', 'ai']
+handler.command = /^(تحسين)$/i
+
+handler.register = false
+handler.limit = false
+
+export default handler
+
+async function processing(urlPath, method) {
+  return new Promise(async (resolve, reject) => {
+    let Methods = ["enhance", "recolor", "dehaze"]
+    Methods.includes(method) ? (method = method) : (method = Methods[0]);
+    let buffer,
+      Form = new FormData(),
+      scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
+    Form.append("model_version", 1, {
+      "Content-Transfer-Encoding": "binary",
+      contentType: "multipart/form-data; charset=uttf-8",
+    });
+    Form.append("image", Buffer.from(urlPath), {
+      filename: "enhance_image_body.jpg",
+      contentType: "image/jpeg",
+    });
+    Form.submit(
+      {
+        url: scheme,
+        host: "inferenceengine" + ".vyro" + ".ai",
+        path: "/" + method,
+        protocol: "https:",
+        headers: {
+          "User-Agent": "okhttp/4.9.3",
+          Connection: "Keep-Alive",
+          "Accept-Encoding": "gzip",
+        },
+      },
+      function (err, res) {
+        if (err) reject();
+        let data = [];
+        res
+          .on("data", function (chunk, resp) {
+            data.push(chunk);
+          })
+          .on("end", () => {
+            resolve(Buffer.concat(data));
+          });
+        res.on("error", (e) => {
+          reject();
+        });
+      }
+    );
+  });
+        }
