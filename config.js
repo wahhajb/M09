6401c1f9e08be4a197372569fda5619b09c7a559
ahmedmazika7 <149413372+ahmedmazika7@users.noreply.github.com>
2024-06-14 04:02:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,20 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['967774318278', '🌩️ ẉa.me//𝑮𝒐𝒌𝒖‖.peị!‽ 👑', true], ['967774318278'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560'], ['967735038560']]
+global.owner = [['212601064446', '🌩️ ẉa.me/AFROTO‖.peị!‽ 👑', true], 
+ ['201067999523', '🌩️ ẉa.me/AFROTO‖.peị!‽ 👑', true],  ['212601064446'], ['201067999523'], ['212601064446'], ['201067999523'], ['201067999523'], ['201067999523'], ['212601064446'], ['201067999523'], ['212601064446']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = '' //Ejemplo: +59309090909
 global.confirmCode = ''
 
-global.animxscans = ['967774318278']
-global.suittag = ['967774318278']
+global.animxscans = ['212601064446']
+global.suittag = ['212601064446']
 global.mods = []
 global.prems = []
 
-global.packname = '『 +967774318278 』'
+global.packname = '『 201067999523 』'
 global.author = '『 غوكـو بــ☠️ـوت 』'
 global.wm = '『غـوكـو بــ🐍ـؤؤت 』'
 global.wm2 = '『 غــوكـو بــ💎ـوت 』'
@@ -28,7 +29,7 @@ global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
 global.dev = '© 𝑮𝒐𝒌𝒖_BOT'
-global.devnum = '+967774318278'
+global.devnum = '212601064446'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
